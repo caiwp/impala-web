0c5e92ad562d1b2865c034c984110ce937ca5184
caiwenpi <caiwenpi@gmail.com>
2017-05-03 20:45:07 +0800

    前端优化

--- main.go
+++ main.go
@@ -8,14 +8,26 @@ import (
 	"github.com/caiwp/impala-web/router"
 	"github.com/caiwp/impala-web/router/middleware"
 	"github.com/gin-gonic/contrib/ginrus"
+	"flag"
+	"fmt"
 )
 
+var (
+	port int
+)
+
+func init() {
+	flag.IntVar(&port, "port", 3000, "listen port")
+}
+
 func main() {
+	flag.Parse()
 	logrus.SetLevel(logrus.DebugLevel)
 
 	handler := router.Load(
 		ginrus.Ginrus(logrus.StandardLogger(), time.RFC3339, false),
 		middleware.Version,
 	)
-	http.ListenAndServe(":3000", handler)
+
+	http.ListenAndServe(fmt.Sprintf(":%d", port), handler)
 }
